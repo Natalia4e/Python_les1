fe494a35c1f1def6553c1f237a872f562ac5da33
Natalia4e <110606894+Natalia4e@users.noreply.github.com>
2022-08-08 17:28:51 +0300

    Delete ex.py

--- ex.py
+++
@@ -1,8 +0,0 @@
-duration = int(input("введите число:"))
-a = duration
-if a== 1 or a== 21 or a== 31 or a== 41 or a== 51 or a== 61 or a== 71 or a== 81 or a== 91 :
-  print(f"{int (a)} процент ", end='' )
-elif a==2 or a==3 or a==4 or a==22 or a==23 or a==24 or a==32 or a==33 or a==34 or a==42 or a==43 or a==44  or a==52 or a==53 or a==54 or a==62 or a==63 or a==64 or a==72 or a==73 or a==74 or a==82 or a==83 or a==84 or a==92 or a==93 or a==94 :
-  print(f"{int (a)} процентa ", end='' )
-else:
-  print(f"{int (a)} процентов ", end='' )
